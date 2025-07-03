e0b9302d9295bbee0f99f4413a59312e9d8e2678
Rapeepan04 <Rapeepantongchai163@gmail.com>
2025-07-03 11:28:45 +0700

    Update Sketch.ino

--- 66030158/1Digit/Sketch.ino
+++ 66030158/1Digit/Sketch.ino
@@ -1,1 +1,36 @@
+const int segmentPins[7] = {19, 18, 5, 17, 16, 4, 0}; // A to G
+
+// Segment patterns for digits 0-9
+const byte digits[10][7] = {
+  // A, B, C, D, E, F, G
+  {1, 1, 1, 1, 1, 1, 0}, // 0
+  {0, 1, 1, 0, 0, 0, 0}, // 1
+  {1, 1, 0, 1, 1, 0, 1}, // 2
+  {1, 1, 1, 1, 0, 0, 1}, // 3
+  {0, 1, 1, 0, 0, 1, 1}, // 4
+  {1, 0, 1, 1, 0, 1, 1}, // 5
+  {1, 0, 1, 1, 1, 1, 1}, // 6
+  {1, 1, 1, 0, 0, 0, 0}, // 7
+  {1, 1, 1, 1, 1, 1, 1}, // 8
+  {1, 1, 1, 1, 0, 1, 1}  // 9
+};
+
+void setup() {
+  for (int i = 0; i < 7; i++) {
+    pinMode(segmentPins[i], OUTPUT);
+  }
+}
+
+void displayDigit(int number) {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(segmentPins[i], digits[number][i]);
+  }
+}
+
+void loop() {
+  for (int i = 0; i <= 9; i++) {
+    displayDigit(i);
+    delay(1000); // แสดงเลขละ 1 วินาที
+  }
+}
 
